53c8313f2cc8ccd9bc3001f6771c76b340b43c4f
renatwork96 <renat.work96@gmail.com>
2021-03-22 23:51:49 +0300

    create: validate mask

--- js/main.js
+++ js/main.js
@@ -64,4 +64,25 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  // Обработка форм
+  $(".form").each(function(){
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "The name must be at least two letters"
+        },
+        email: {
+          required: "Please specify your email",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "Please specify your phone number",
+        },
+      }
+    });
+  })
+  $(".phone").mask("+7 (999) 999-99-99");
+
 });
