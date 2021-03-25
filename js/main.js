29dcb4077fca688c2a3911e0387ef7044feec1b6
renatwork96 <renat.work96@gmail.com>
2021-03-25 19:49:45 +0300

    checkpoint, bug fixes

--- js/main.js
+++ js/main.js
@@ -86,4 +86,26 @@ $(document).ready(function () {
   })
   $(".phone").mask("+7 (999) 999-99-99");
   AOS.init();
+
+  let map = document.getElementsByClassName('map')[0];
+    let options_map = {
+        once: true,
+        passive: true,
+        capture: true
+    };
+    map.addEventListener('click', start_lazy_map, options_map);
+    map.addEventListener('mouseover', start_lazy_map, options_map);
+    map.addEventListener('touchstart', start_lazy_map, options_map);
+    map.addEventListener('touchmove', start_lazy_map, options_map);
+
+    let map_loaded = false;
+    function start_lazy_map() {
+        if (!map_loaded) {
+            let map_block = document.getElementsByClassName('map__yandex')[0];
+            map_loaded = true;
+            map_block.setAttribute('src', map_block.getAttribute('data-src'));
+            map_block.removeAttribute('data-src');
+        }
+    }
+
 });
